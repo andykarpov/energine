0e477da3eb9013399f54feb21b81761a9bcf808d
Andy Karpov <andy.karpov@gmail.com>
2015-11-17 23:28:08 +0200

    модификации для понижения зависимости php >= 5.4

--- core/modules/user/components/RoleEditor.php
+++ core/modules/user/components/RoleEditor.php
@@ -207,7 +207,8 @@ class RoleEditor extends Grid {
      */
     // При удалении происходит проверка не удаляется ли дефолтная группа
     protected function deleteData($id) {
-        if (!empty($this->dbh->select($this->getTableName(), 'group_id', ['group_id' => $id, 'group_default' => true]))) {
+        $default_group = $this->dbh->select($this->getTableName(), 'group_id', ['group_id' => $id, 'group_default' => true]);
+        if (!empty($default_group)) {
             throw new SystemException('ERR_DEFAULT_GROUP', SystemException::ERR_NOTICE);
         }
         parent::deleteData($id);
